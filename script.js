abf3610031bdd1c881479ddbd71a643115bf3c99
Sreshta-Siliveri <sreshtasiliveri@gmail.com>
2025-02-05 12:43:06 +0530

    AirlineSentimentAnalysis

---
+++ script.js
@@ -0,0 +1,5 @@
+// JS for the button interaction
+document.getElementById("predictButton").addEventListener("click", function() {
+    alert("Sentiment prediction is in progress...");
+    // Add functionality here to predict sentiment using your API or backend logic
+});
